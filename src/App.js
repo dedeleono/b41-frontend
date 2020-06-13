c273589beef08cb52c7d09cd830333e5f6676596
Diego de Leon <dienrique24@hotmail.com>
2020-06-12 20:53:02 -0500

    Agregar ej

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from './components/Nav'
 
 function App() {
   return (
-    <React.fragment>
+    <React.Fragment>
+      <Navigation />
       <h1>Que onda raza</h1>
-    </React.fragment>
+    </React.Fragment>
   
   );
 }
